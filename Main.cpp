cf0c47a71102b89ccef44d6d4ddc26b1a34738f9
Cole <coltoncoey13@gmail.com>
2023-11-29 10:32:12 -0500

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+// Colton Coey
+// CIS 1202 501
+// November 29, 2023
+
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+template <typename T>
+T half(T value) {
+	return value / 2;
+}
